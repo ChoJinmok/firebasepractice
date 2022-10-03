66554630f0d4cf625a4138e0e22a08cfdb224473
ChoJinmok <jjmok1127@gmail.com>
2022-10-03 19:39:34 +0900

    Router Setup

---
+++ src/components/Router.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+import Profile from '../routes/Profile';
+import EditProfile from '../routes/EditProfile';
+
+export default function Router() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Profile />} />
+            <Route path="/" element={<EditProfile />} />
+          </>
+        ) : (
+          <Route path="/" element={<Auth />} />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react';
 
 import ReactDom from 'react-dom/client';
 
-import App from './App';
-
-import firebase from './firebase';
-
-console.log(firebase);
+import App from './components/App';
 
 ReactDom.createRoot(document.getElementById('app')).render(
   <StrictMode>
