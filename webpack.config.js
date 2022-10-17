6af15c7cd94d32d5b3fcf02ff0d6d0cb7d4919e9
ChoJinmok <jjmok1127@gmail.com>
2022-10-18 01:45:10 +0900

    Test: 배포 테스트

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = () => {
       }),
     ],
     output: {
-      path: path.resolve(__dirname, 'docs'),
+      path: path.resolve(__dirname, 'docs/firebasepractice'),
       filename: 'app.js',
     },
   };
